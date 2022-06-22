b5eb82b7b64dfb4eea3dae73188a40eb34f4814c
Hamid V. Mammadov <Hamid.mammadov@code.local>
2022-06-22 12:54:43 +0400

    First Commit

---
+++ P129Allup/P129Allup/Areas/Manage/Controllers/CategoryController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using P129Allup.DAL;
+using P129Allup.Models;
+using P129Allup.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace P129Allup.Areas.Manage.Controllers
+{
+    [Area("manage")]
+    public class CategoryController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public CategoryController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index(int? status, int page = 1)
+        {
+            IQueryable<Category> query = _context.Categories;
+
+            if (status != null && status > 0)
+            {
+                if (status == 1)
+                {
+                    query = query.Where(b => b.IsDeleted);
+                }
+                else if (status == 2)
+                {
+                    query = query.Where(b => !b.IsDeleted);
+                }
+            }
+
+            int itemCount = int.Parse(_context.Settings.FirstOrDefault(s => s.Key == "PageItemCount").Value);
+            ViewBag.Status = status;
+
+            return View(PageNatedLisd<Category>.Create(query, page, itemCount));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            ViewBag.MainCategories = await _context.Categories.Where(c => !c.IsDeleted && c.IsMain).ToListAsync();
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Category category)
+        {
+            ViewBag.MainCategories = await _context.Categories.Where(c => !c.IsDeleted && c.IsMain).ToListAsync();
+
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            if (category.IsMain && category.Image == null)
+            {
+                ModelState.AddModelError("Image", "Main Categoriada Sekil Mutleqdir");
+                return View();
+            }
+            else
+            {
+                if (category.ParentId == null || !await _context.Categories.AnyAsync(c=>!c.IsDeleted && c.IsMain && c.Id == category.ParentId))
+                {
+                    ModelState.AddModelError("ParentId", "Parent Duzgun Secin");
+                    return View();
+                }
+            }
+
+            if (await _context.Categories.AnyAsync(c=>!c.IsDeleted && c.Name == category.Name.Trim().ToLower()))
+            {
+                ModelState.AddModelError("Name", "Already Exist");
+                return View();
+            }
+
+            category.Name = category.Name.Trim();
+            category.CreatedAt = DateTime.UtcNow.AddHours(4);
+
+            await _context.Categories.AddAsync(category);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("index");
+        }
+    }
+}
---
+++ P129Allup/P129Allup/wwwroot/manage/js/custom.js
@@ -0,0 +1,105 @@
+﻿$(document).ready(function () {
+    $(document).on('click', '.deleteBtn', function (e) {
+        e.preventDefault();
+
+
+        Swal.fire({
+            title: 'Eminsen?',
+            text: "Bunu Geri Qaytara Bilmiyecen!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            cancelButtonText:"Silme",
+            confirmButtonText: 'Beli, Sil!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+                let url = $(this).attr('href');
+
+                fetch(url)
+                    .then(res => res.text())
+                    .then(data => { $('.tblContent').html(data) });
+
+                Swal.fire(
+                    'Silindi!',
+                    'Secdiyniz Brand Silindi.',
+                    'success'
+                )
+            }
+        })
+    })
+
+    $(document).on('click', '.restoreBtn', function (e) {
+        e.preventDefault();
+
+        Swal.fire({
+            title: 'Eminsen?',
+            text: "Bunu Geri Qaytara Bilmiyecen!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            cancelButtonText: "Silme",
+            confirmButtonText: 'Beli, Sil!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+                let url = $(this).attr('href');
+
+                fetch(url)
+                    .then(res => res.text())
+                    .then(data => { $('.tblContent').html(data) });
+
+                Swal.fire(
+                    'Silindi!',
+                    'Secdiyniz Brand Silindi.',
+                    'success'
+                )
+            }
+        })
+    })
+
+    if ($('.isMaininput').is(":checked")) {
+        $('.imagecontainer').removeClass('d-none');
+        $('.parentcontainer').addClass('d-none');
+    } else {
+        $('.imagecontainer').addClass('d-none');
+        $('.parentcontainer').removeClass('d-none');
+    }
+
+    $(document).on('change', '.isMaininput', function () {
+        console.log($(this).is(":checked"))
+        if ($(this).is(":checked")) {
+            $('.imagecontainer').removeClass('d-none');
+            $('.parentcontainer').addClass('d-none');
+        } else {
+            $('.imagecontainer').addClass('d-none');
+            $('.parentcontainer').removeClass('d-none');
+        }
+    })
+
+    
+
+    toastr.options = {
+        "closeButton": true,
+        "debug": false,
+        "newestOnTop": false,
+        "progressBar": true,
+        "positionClass": "toast-top-right",
+        "preventDuplicates": false,
+        "onclick": null,
+        "showDuration": "300",
+        "hideDuration": "1000",
+        "timeOut": "5000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+    }
+
+    if ($('#successInput').val().length) {
+        toastr["success"]($('#successInput').val(), $('#successInput').val().split(' ')[0])
+    }
+})
